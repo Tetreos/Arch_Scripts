4eea6d9278fe7edb9db5ba44906c3986c210c769
Maxim <49373922+Tetreos@users.noreply.github.com>
2021-07-02 21:03:16 +0200

    Update chroot_script.sh

--- chroot_script.sh
+++ chroot_script.sh
@@ -5,6 +5,7 @@ hwclock --systohc
 pacman nano
 
 sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 
 echo KEYMAP="de-latin1 " > /etc/vconsole.conf
@@ -37,8 +38,6 @@ mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S networkmanager vim git
+pacman -S networkmanager
 
 systemctl enable NetworkManager
-
-exit

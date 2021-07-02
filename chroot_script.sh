0941949fea341c37c8465d9a87d39ef5b8aa2c0a
Maxim <49373922+Tetreos@users.noreply.github.com>
2021-07-02 20:19:51 +0200

    Update chroot_script.sh

--- chroot_script.sh
+++ chroot_script.sh
@@ -4,4 +4,26 @@ hwclock --systohc
 
 pacman nano
 
-nano /etc/locale.gen
+sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+echo KEYMAP="de-latin1 " > /etc/vconsole.conf
+
+echo "archlinux" > /etc/hostname
+
+cat << EOF >> /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   archlinux.localdomain   archlinux
+EOF
+
+passwd
+a
+
+useradd -m tetra
+passwd tetra
+a
+
+usermod -aG wheel,audio,video,optical,storage tetra
+
+pacman -S sudo

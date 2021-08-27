e8a6bb448ca6695bb5f4264abeda49829fd03730
Maxim <49373922+Tetreos@users.noreply.github.com>
2021-08-27 18:46:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-# Arch Install
+source config.sh
+# Pre-installation --------------------------------------------------------------------------------------------------
 
 # Load the right Keymap
-echo "Select the right Keymap"
-KEYMAP="de-latin1"
+echo "Setting the keyboard layout"
 loadkeys $KEYMAP
 
-# Systemclock
-echo "System Clock"
+# Verify the boot mode
+# If the command shows the directory without error, then the system is booted in UEFI mode
+# If the directory does not exist, the system may be booted in BIOS (or CSM) mode
+echo "Verifying the boot mode"
+echo "If the command shows the directory without error, then the system is booted in UEFI mode"
+echo "If the directory does not exist, the system may be booted in BIOS (or CSM) mode"
+ls /sys/firmware/efi/efivars
+sleep 5
+
+# Connect to the internet
+# Not necessary when Ethernet
+# TO-DO Wi-Fi
+
+# Update the system clock
+echo "Updating the system clock"
 timedatectl set-ntp true
 
-# Drive
-echo "Partitioning drive"
+# TO-DO -----------------------------------
+# Partition the disk /dev/sda
+echo "Partitioning the disk /dev/sda"
 (
 echo g
 echo n # Add a new partition
@@ -33,23 +47,49 @@ echo   # First sector (Accept default: 1)
 echo 
 echo w # Write changes
 ) | fdisk /dev/sda
+# TO-DO -----------------------------------
 
-echo "MKFS"
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
+# Format the partitions
+echo "Ext4 file system on /dev/sda"
 mkfs.ext4 /dev/sda3
 
-echo "Mount sda3"
+echo "Initializing swap"
+mkswap /dev/sda2
+
+mkfs.fat -F32 /dev/sda1
+
+# Mounting the file systems
+echo "Mounting root volume sda3"
 mount /dev/sda3 /mnt
+
+echo "Enabling swap"
+swapon /dev/sda2
+
+# Installation ------------------------------------------------------------------------------------------------------
+
+# Select the mirrors
+# TO-DO
+
+# Installing essential packages
 pacstrap /mnt base linux linux-firmware
 
+# Configure the system ----------------------------------------------------------------------------------------------
+
+# Generating fstab file
+echo "Generating fstab file"
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Chroot
 echo "Chroot"
-sleep 5
 cp chroot_script.sh /mnt
-arch-chroot /mnt
-rm /mnt/chroot_script.sh
+cp config.sh /mnt
 
+read -n 1 -s -r -p "Press any key to continue"
+
+arch-chroot /mnt
+
+# After Chroot
+
+rm /mnt/chroot_script.sh
+rm /mnt/config.sh
 umount -l /mnt
